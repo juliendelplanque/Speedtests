b27c17a0bfdcbd47e09f1b7d9ea2edd2b3983b61
Raspberry-pi <>
2016-06-09 14:37:20 +0200

    Removed comments.

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -3,12 +3,12 @@
 GIT_REPOSITORY="/home/julien/Speedtests"
 FILE="$GIT_REPOSITORY/data/speedtest_$(date +%d.%m.%Y-%T).txt"
 
-wget -q --spider http://google.com && \ # Check if connected to internet.
+wget -q --spider http://google.com && \
 echo "Online. Doing a speed test..." && \
-/usr/bin/speedtest --simple > $FILE && \ # Do the speed test.
+/usr/bin/speedtest --simple > $FILE && \
 cd $GIT_REPOSITORY && \
-git pull && \ # Pull eventual changes to the repository.
-git add data && \ # Add all new files in data directory.
-git commit -am "Automatically added speedtest." && \ # Commit new files.
+git pull && \
+git add data && \
+git commit -am "Automatically added speedtest." && \
 git push
 
